b0d3dd9c212ec7d5c2d3160f2e6c0bc5cc803b65
Iamsorry <code.project@m2k.com.tw>
2016-06-18 20:12:31 +0800

    Renamed casino

--- src/main/scala/idv/sorry/Sibala.scala
+++ src/main/scala/idv/sorry/Sibala.scala
@@ -4,15 +4,14 @@ import akka.actor.{ Actor, ActorRef, ActorSystem, Props }
 
 object Sibala {
 	def main(args: Array[String]) {
-
 		println("Sibala!")
 
-		val bicycle = ActorSystem("Sibala")
-		val banker = bicycle.actorOf(Props(classOf[Banker], "abeh"))
+		val casino = ActorSystem("bicycle")
+		val banker = casino.actorOf(Props(classOf[Banker], "abeh"))
 		var players = List[ActorRef]()
 
 		for (i <- 0 to 9) {
-			players = bicycle.actorOf(Props(classOf[Player], s"adia${i}", banker)) :: players
+			players = casino.actorOf(Props(classOf[Player], s"adia${i}", banker)) :: players
 		}
 
 		players.foreach(_ ! Play)
