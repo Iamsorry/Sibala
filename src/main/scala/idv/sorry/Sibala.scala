97eaa8b1839f67c761fe1e30f015d57a0ee03202
Iamsorry <code.project@m2k.com.tw>
2016-06-20 10:24:35 +0800

    Shutdown actor system irresponsibly

--- src/main/scala/idv/sorry/Sibala.scala
+++ src/main/scala/idv/sorry/Sibala.scala
@@ -3,20 +3,21 @@ package idv.sorry
 import akka.actor.{ Actor, ActorRef, ActorSystem, Props }
 
 object Sibala {
-	def main(args: Array[String]) {
-		println("Sibala!")
+    def main(args: Array[String]) {
+        println("Sibala!")
 
-		val casino = ActorSystem("bicycle")
-		val banker = casino.actorOf(Props(classOf[Banker], "abeh"))
-		var players = List[ActorRef]()
+        val casino = ActorSystem("bicycle")
+        val banker = casino.actorOf(Props(classOf[Banker], "abeh"))
+        var players = List[ActorRef]()
 
-		for (i <- 0 to 9) {
-			players = casino.actorOf(Props(classOf[Player], s"adia$i", banker)) :: players
-		}
+        for (i <- 0 to 9) {
+            players = casino.actorOf(Props(classOf[Player], s"adia$i", banker)) :: players
+        }
 
-		players.foreach(_ ! Play)
+        players.foreach(_ ! Play)
 
-		Thread.sleep(1000 * 4)
-		println("Sayonara")
-	}
+        Thread.sleep(1000 * 4)
+        println("Sayonara")
+        casino.terminate()
+    }
 }
